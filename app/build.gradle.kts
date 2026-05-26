1caaca7b0442c1c5a26dde60ba3ab6033997bc83
Andrzej Dopierała <undefine@aramin.net>
2026-05-26 22:57:06 +0200

    chore: bump version to 0.1.25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.theundefined.omnis"
         minSdk = 26
         targetSdk = 34
-        versionCode = 26
-        versionName = "0.1.24"
+        versionCode = 27
+        versionName = "0.1.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
