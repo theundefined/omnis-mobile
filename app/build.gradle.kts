a52c1291fe2622296ec580044f60c091f5235354
Andrzej Dopierała <undefine@aramin.net>
2026-05-26 22:35:21 +0200

    Emergency fix: disable R8 minification to restore full functionality in Release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             
             val password = System.getenv("RELEASE_KEYSTORE_PASSWORD")
