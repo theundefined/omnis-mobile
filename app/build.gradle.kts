31e447f5860f7d9f165e2392b2423e6b61d955f3
Andrzej Dopierała <undefine@aramin.net>
2026-08-04 22:53:03 +0200

    release: v0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.theundefined.omnis"
         minSdk = 26
         targetSdk = 35
-        versionCode = 34
-        versionName = "0.4.1"
+        versionCode = 35
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
